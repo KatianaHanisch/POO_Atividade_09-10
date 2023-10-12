c8152d80358f25c8b331c3689766e59db0995a1a
Iago <100394244+IagoAparecido@users.noreply.github.com>
2023-10-12 16:40:28 -0400

    funcionante

--- src/main/java/com/apicomsqlite/poo/service/VendaService.java
+++ src/main/java/com/apicomsqlite/poo/service/VendaService.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.apicomsqlite.poo.enity.Estoque;
 import com.apicomsqlite.poo.enity.Venda;
+import com.apicomsqlite.poo.repository.EstoqueRepository;
 import com.apicomsqlite.poo.repository.VendaRepository;
 import jakarta.transaction.Transactional;
 
@@ -13,19 +16,60 @@ public class VendaService {
 
     @Autowired(required = false)
     private VendaRepository vendaRepository;
+    @Autowired(required = false)
+    private EstoqueRepository estoqueRepository;
 
     @Transactional
+    // public String createVenda(Venda venda) {
+    // try {
+    // if (!vendaRepository.existsById(venda.getId())) {
+    // venda.setId(null == vendaRepository.findMaxId() ? 1 :
+    // vendaRepository.findMaxId() + 1);
+    // vendaRepository.save(venda);
+    // return "venda cadastrado com sucesso.";
+    // } else {
+    // return "venda já existe no banco.";
+    // }
+    // } catch (Exception e) {
+    // throw e;
+    // }
+    // }
+
     public String createVenda(Venda venda) {
         try {
             if (!vendaRepository.existsById(venda.getId())) {
                 venda.setId(null == vendaRepository.findMaxId() ? 1 : vendaRepository.findMaxId() + 1);
                 vendaRepository.save(venda);
-                return "venda cadastrado com sucesso.";
+
+                atualizarEstoque(venda);
+
+                return "Venda cadastrada com sucesso.";
             } else {
-                return "venda já existe no banco.";
+                return "Venda já existe no banco.";
             }
         } catch (Exception e) {
             throw e;
+        }
+    }
+
+    private void atualizarEstoque(Venda venda) {
+        List<Estoque> estoqueList = estoqueRepository.findByIdProduto(venda.getIdProduto());
+
+        if (!estoqueList.isEmpty()) {
+            Estoque estoque = estoqueList.get(0);
+
+            int quantidadeAtual = estoque.getQuantidadeProduto();
+            int quantidadeVendida = venda.getQuantidade();
+
+            if (quantidadeAtual >= quantidadeVendida) {
+                int novaQuantidade = quantidadeAtual - quantidadeVendida;
+                estoque.setQuantidadeProduto(novaQuantidade);
+                estoqueRepository.save(estoque);
+            } else {
+                throw new RuntimeException("Quantidade insuficiente no estoque.");
+            }
+        } else {
+            throw new RuntimeException("Produto não encontrado no estoque.");
         }
     }
 
